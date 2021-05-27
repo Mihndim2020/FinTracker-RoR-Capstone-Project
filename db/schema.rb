f5bb8c77a067540c286ceeeafe599ae90c9562d6
Mihndim <jelsieprisca2016@gmail.com>
2021-05-27 17:14:07 -0400

    Migrate expenditures database

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_27_173216) do
+ActiveRecord::Schema.define(version: 2021_05_27_211112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "expenditures", force: :cascade do |t|
+    t.string "name"
+    t.integer "author_id"
+    t.integer "group_id"
+    t.integer "amount"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
