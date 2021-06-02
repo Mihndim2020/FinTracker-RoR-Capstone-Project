234f7082b3c8682ec5393991428dd4d7df1e4d4f
Mihndim <jelsieprisca2016@gmail.com>
2021-06-02 11:56:08 -0400

    Add expenditures index page

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,8 @@
 require_relative "boot"
 
+
+require_relative 'boot'
+
 require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
@@ -10,6 +13,7 @@ module Fintracker
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.serve_static_assets = true
 
     # Configuration for the application, engines, and railties goes here.
     #
